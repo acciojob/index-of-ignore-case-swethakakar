c0e73250fdbd99124cd10c2fd966c643ed2422ba
Swetha <swethakakar@gmail.com>
2025-01-09 15:37:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
-	if(s1.includes(s2)){
-		let index=s1.indexOf(s2);
-		return index;
-	}
-	
-	else {
-		return -1;
-	}
+	if (s1 === '' || s2 === '') {
+        return -1;
+    }
+
+	s1 = s1.toLowerCase();
+    s2 = s2.toLowerCase();
+    // Check if s1 includes s2 (case-sensitive)
+    if (s1.includes(s2)) {
+        let index = s1.indexOf(s2);
+        return index;
+    } else {
+        return -1;
+    }
+
 }
 // Please do not change the code below
-const s1 = prompt("Enter s1:"); 
-const s2 = prompt("Enter s2:");
+const s1 = prompt("Enter s1:"); const s2 = prompt("Enter s2:");
 alert(indexOfIgnoreCase(s1, s2));
